ad3af8b0e2e8a0b57374606b7b9061c85974be41
kejiahp <popoolakejiah@gmail.com>
2023-12-02 22:54:33 +0100

    inital commit

---
+++ src/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AppService {
+  constructor (private readonly configService:ConfigService) {}
+  healthCheck(): string {
+    return 'Server is running on port: '+ this.configService.get<string>("PORT");
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT') || 5000;
+  await app.listen(PORT);
+}
+bootstrap();
